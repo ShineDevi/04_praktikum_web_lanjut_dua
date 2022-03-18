cb6c9f30d1a22f85f60b59f72feeec4ba5cf6c40
ShineDevi <shinedevi02@gmail.com>
2022-03-18 16:34:04 +0700

    lupa inisialisasi dulu sebelum mengerjakan

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Pegawai;
+
+class PegawaiController extends Controller
+{
+    public function index()
+    {
+    	$pegawai = Pegawai::all();
+    	return view('pegawai', ['pegawai' => $pegawai]);
+        // $pegawai = Pegawai::first();  
+	    // return view('pegawai', ['pegawai' => $pegawai]);
+    }
+}
---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class PegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        for($x=1; $x<=10; $x++){
+            DB::table('pegawai')->insert([
+                'nama' => $faker->name,
+                'alamat' => $faker->address,
+            ]);
+        }
+    }
+}
